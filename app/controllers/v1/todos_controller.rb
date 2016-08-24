c98ce2b19bd0142428cbe3b46e78f52917e06f13
Patel Harshad <patelharshad1@gmail.com>
2016-08-24 17:54:21 +0530

    changes in json structure

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -2,7 +2,7 @@ module V1
 	class TodosController < ApplicationController
 
 		def index
-		 	file = File.read("#{Rails.root}/public/v1/prototype/data/online_users.json")
+		 	file = File.read("#{Rails.root}/public/api/v1/prototype/data/online_users.json")
   		data = JSON.parse(file)
 			render json: {data: data}
 		end
